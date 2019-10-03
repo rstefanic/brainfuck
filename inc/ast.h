b93335cb7a7d0c9bfcdc49577e45317184c7e1ae
robert <rstefanic72@gmail.com>
2019-10-03 13:35:44 -0500

    Updated AST Instructions and Node

--- inc/ast.h
+++ inc/ast.h
@@ -8,39 +8,20 @@ namespace AST {
         DECREMENT_BYTE,
         OUTPUT_BYTE,
         INPUT_BYTE,
-        BYTE_EQ_ZERO,
-        BYTE_NEQ_ZERO 
-    };
-    
-    class Expression {};
-    
-    class ValExpression : public Expression {
-    public:
-        int amount = 0;
-        void increment() { amount++; };
-        void decrement() { amount--; };
-    };
-
-    class IncDataPtr : public ValExpression {};
-    class DecDataPtr : public ValExpression {};
-    class IncByte : public ValExpression {};
-    class DecByte : public ValExpression {};
-    class OutputByte : public Expression {};
-    class InputByte : public Expression {};
-    
-    class Loop : public Expression {
-    private:
-        std::vector<Expression*> expressions;
+        LOOP
+        // BYTE_EQ_ZERO,
+        // BYTE_NEQ_ZERO 
     };
     
     class Node {
     public:
         Node(INSTRUCTION instruction) :
             _instruction(instruction) {};
+            
         void increment() { _value++; }
-        void decrement() { _value--; }
         INSTRUCTION instruction() { return _instruction; }
         int value() { return _value; }
+        std::vector<Node*> subexpressions;
         
     private:
         INSTRUCTION _instruction;
