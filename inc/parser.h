dabe985e9f05601f45343991fbd60ed66e201e69
robert <rstefanic72@gmail.com>
2019-10-03 13:36:52 -0500

    Updated Parser to parse BF loops

--- inc/parser.h
+++ inc/parser.h
@@ -1,15 +1,18 @@
+#include <stdlib.h>
+
 #include "ast.h"
 #include "lexer.h"
 #include <vector>
 
 namespace Parser 
 {
-    bool parse(BF_TOKEN* tokens);
-    AST::Expression* parse_inc_data_ptr(BF_TOKEN** t_pos);
-    AST::Expression* parse_dec_data_ptr(BF_TOKEN** t_pos);
-    AST::Expression* parse_inc_byte(BF_TOKEN** t_pos);
-    AST::Expression* parse_dec_byte(BF_TOKEN** t_pos);
-    AST::Expression* parse_output_byte(BF_TOKEN** t_pos);
-    AST::Expression* parse_input_byte(BF_TOKEN** t_pos);
-    AST::Expression* parse_loop(BF_TOKEN** t_pos);
+    std::vector<AST::Node*> parse(BF_TOKEN* tokens);
+    AST::Node* parse_inc_data_ptr(BF_TOKEN** t_pos);
+    AST::Node* parse_dec_data_ptr(BF_TOKEN** t_pos);
+    AST::Node* parse_inc_byte(BF_TOKEN** t_pos);
+    AST::Node* parse_dec_byte(BF_TOKEN** t_pos);
+    AST::Node* parse_output_byte(BF_TOKEN** t_pos);
+    AST::Node* parse_input_byte(BF_TOKEN** t_pos);
+    AST::Node* parse_loop(BF_TOKEN** t_pos);
+    std::string parse_error(std::string error_message);
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,9 +1,10 @@
 #include "../inc/parser.h"
 
-namespace Parser {
-    bool parse(BF_TOKEN* tokens) {
-        
-        std::vector<AST::Expression*> expressions;
+namespace Parser 
+{
+    std::vector<AST::Node*> parse(BF_TOKEN* tokens) 
+    {
+        std::vector<AST::Node*> expressions;
         
         for (BF_TOKEN** token = &tokens; **token != INVALID_TOKEN;) {
             switch(**token) {
@@ -29,7 +30,7 @@ namespace Parser {
                     expressions.push_back(parse_loop(token));
                     break;
                 case RIGHT_BRACKET_TOKEN:
-                    return false;
+                    parse_error("Unmatched left bracket to start this right bracket.");
                 case COMMENT_TOKEN:
                     (*token)++;
                 case INVALID_TOKEN:
@@ -37,35 +38,36 @@ namespace Parser {
             }
         }
 
-        return true;
+        return expressions;
     }
 
-    AST::Expression* parse_inc_data_ptr(BF_TOKEN** token) {
-        AST::IncDataPtr* inc_data_ptr = new AST::IncDataPtr();
+    AST::Node* parse_inc_data_ptr(BF_TOKEN** token) 
+    {
+        AST::Node* inc_data_ptr = new AST::Node(AST::INCREMENT_DATA_PTR);
 
         while(**token == RIGHT_ANGLE_TOKEN) {
             inc_data_ptr->increment();
             (*token)++;
         }
-        
-        std::cout << "inc dptr \n";
 
         return inc_data_ptr;
     }
     
-    AST::Expression* parse_dec_data_ptr(BF_TOKEN** token) {
-        AST::DecDataPtr* dec_data_ptr = new AST::DecDataPtr();
+    AST::Node* parse_dec_data_ptr(BF_TOKEN** token) 
+    {
+        AST::Node* dec_data_ptr = new AST::Node(AST::DECREMENT_DATA_PTR);
 
         while(**token == LEFT_ANGLE_TOKEN) {
-            dec_data_ptr->decrement();
+            dec_data_ptr->increment();
             (*token)++;
         }
 
         return dec_data_ptr;
     }
     
-    AST::Expression* parse_inc_byte(BF_TOKEN** token) {
-        AST::IncByte* inc_byte = new AST::IncByte();
+    AST::Node* parse_inc_byte(BF_TOKEN** token) 
+    {
+        AST::Node* inc_byte = new AST::Node(AST::INCREMENT_BYTE);
 
         while(**token == PLUS_TOKEN) {
             inc_byte->increment();
@@ -75,31 +77,80 @@ namespace Parser {
         return inc_byte;
     }
     
-    AST::Expression* parse_dec_byte(BF_TOKEN** token) {
-        AST::DecByte* dec_byte = new AST::DecByte();
+    AST::Node* parse_dec_byte(BF_TOKEN** token) 
+    {
+        AST::Node* dec_byte = new AST::Node(AST::DECREMENT_BYTE);
 
         while(**token == MINUS_TOKEN) {
-            dec_byte->decrement();
+            dec_byte->increment();
             (*token)++;
         }
 
         return dec_byte;
     }
     
-    AST::Expression* parse_output_byte(BF_TOKEN** token) {
-        AST::OutputByte* output_byte = new AST::OutputByte();
+    AST::Node* parse_output_byte(BF_TOKEN** token) 
+    {
+        AST::Node* output_byte = new AST::Node(AST::OUTPUT_BYTE);
         (*token)++;
         return output_byte;
     }
     
-    AST::Expression* parse_input_byte(BF_TOKEN** token) {
-        AST::InputByte* input_byte = new AST::InputByte();
+    AST::Node* parse_input_byte(BF_TOKEN** token) 
+    {
+        AST::Node* input_byte = new AST::Node(AST::INPUT_BYTE);
         (*token)++;
         return input_byte;
     }
     
-    AST::Expression* parse_loop(BF_TOKEN** token) {
-        (*token)++;
-        return new AST::Loop();
+    AST::Node* parse_loop(BF_TOKEN** token) 
+    {
+        AST::Node* loop = new AST::Node(AST::LOOP);
+        (*token)++; // parse the first left_bracket_token out
+        
+        while(**token != RIGHT_BRACKET_TOKEN && **token != INVALID_TOKEN) {
+            switch(**token) {
+                case RIGHT_ANGLE_TOKEN:
+                    loop->subexpressions.push_back(parse_inc_data_ptr(token));
+                    break;
+                case LEFT_ANGLE_TOKEN:
+                    loop->subexpressions.push_back(parse_dec_data_ptr(token));
+                    break;
+                case PLUS_TOKEN:
+                    loop->subexpressions.push_back(parse_inc_byte(token));
+                    break;
+                case MINUS_TOKEN:
+                    loop->subexpressions.push_back(parse_dec_byte(token));
+                    break;
+                case PERIOD_TOKEN:
+                    loop->subexpressions.push_back(parse_output_byte(token));
+                    break;
+                case COMMA_TOKEN:
+                    loop->subexpressions.push_back(parse_input_byte(token));
+                    break;
+                case LEFT_BRACKET_TOKEN:
+                    loop->subexpressions.push_back(parse_loop(token));
+                   break;
+                case RIGHT_BRACKET_TOKEN:
+                    parse_error("ERR: Unmatched Right Bracket");
+                case COMMENT_TOKEN:
+                    (*token)++;
+                case INVALID_TOKEN:
+                    break;
+            }
+        }
+        
+        if (**token == RIGHT_BRACKET_TOKEN) {
+            (*token)++;
+        }
+        
+        return loop;
+    }
+    
+    std::string parse_error(std::string error_description) 
+    {
+        std::cout << "An error occured during parsing\n";
+        std::cout << error_description << "\n";
+        exit(EXIT_FAILURE);
     }
 }
