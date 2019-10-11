f80d1f481a5eac10dc9c2885fcb6dc0a3f3158f9
robert <rstefanic72@gmail.com>
2019-10-11 17:30:29 -0500

    Added REPL to program

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include "inc/main.h"
 #include "inc/parser.h"
 
-void handle_loop_print(AST::Node* node);
+void run_repl(std::istream& input);
+int count_open_braces(std::string line);
 
 int main(int argc, char** argv) 
 {
@@ -23,9 +24,63 @@ int main(int argc, char** argv)
         }
     }
     else {
-        std::cout << "No file given as an argument.\n";
+        run_repl(std::cin);
         return -1;
     }
 
     return 0;
+}
+
+void run_repl(std::istream& input)
+{
+    const std::string new_command_prompt = ">> ";
+    const std::string continuing_command_prompt = ".. ";
+    bool running = true;
+
+    std::cout << "Brainfuck 1.0.0\n";
+    std::cout << "Type \"quit\" to quit.\n";
+
+    while (running) {
+        std::string code;
+        int open_braces = 0;
+
+        std::cout << new_command_prompt;
+
+        for (std::string line; std::getline(input, line);) {
+
+            if (line.compare("quit") == 0) {
+                return;
+            }
+
+            code += line;
+
+            open_braces += count_open_braces(line);
+            if (open_braces == 0) {
+                break;
+            }
+
+            std::cout << continuing_command_prompt;
+        }
+
+        std::stringstream ss;
+        ss.str(code);
+        BF_TOKEN* tokens = Lexer::lex(ss);
+        std::vector<AST::Node*> ast = Parser::parse(tokens);
+        auto evaluator = new Interpreter::Environment(ast);
+        evaluator->Eval();
+    }
+}
+
+int count_open_braces(std::string line)
+{
+    int open_braces = 0;
+
+    for(char& c: line) {
+        switch(c) {
+            case '[': open_braces++; break;
+            case ']': open_braces--; break;
+        }
+    }
+
+    return open_braces;
 }
